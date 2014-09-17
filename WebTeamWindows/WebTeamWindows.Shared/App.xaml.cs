eb73ae906e33fff85e9413e4a90c5e9e7a2a78ab
Thomas Havy <grumluluche@gmail.com>
2014-09-17 17:17:18 +0200

    Added some old theme files

--- WebTeamWindows/WebTeamWindows.Shared/App.xaml.cs
+++ WebTeamWindows/WebTeamWindows.Shared/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using WebTeam_ENSEA_Universal;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
--- WebTeamWindows/WebTeamWindows.WindowsPhone/MainPage.xaml.cs
+++ WebTeamWindows/WebTeamWindows.WindowsPhone/MainPage.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,10 +14,12 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using WebTeam_ENSEA_Universal.Resources;
+using Windows.UI.ViewManagement;
 
 // Pour en savoir plus sur le modèle d'élément Page vierge, consultez la page http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace WebTeamWindows
+namespace WebTeam_ENSEA_Universal
 {
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
@@ -44,5 +48,145 @@ namespace WebTeamWindows
             // Si vous utilisez le NavigationHelper fourni par certains modèles,
             // cet événement est géré automatiquement.
         }
+
+        #region Caligula
+
+        private async void BeginCaligulaLogin()
+        {
+            /*HttpWebRequest client = (HttpWebRequest)WebRequest.Create(APICaligula.caligulaUrl);
+
+            //Création du dictionnaire contenant les valeurs post
+            Dictionary<string, string> queryString = new Dictionary<string, string>();
+            queryString.Add("login", "ensea");
+            queryString.Add("password", "ensea");
+            string postdata = "";
+            foreach (KeyValuePair<string, string> key in queryString)
+            {
+                postdata += key.Key + "=" + key.Value + "&";
+            }
+
+            var postBytes = APICaligula.StringToAscii(postdata);
+
+            client.CookieContainer = new CookieContainer();
+            client.Method = "POST";
+            client.ContentType = "application/x-www-form-urlencoded";
+            client.ContentLength = postBytes.Length;
+
+
+            client.BeginGetResponse(resultat =>
+            {
+                HttpWebRequest request = (HttpWebRequest)resultat.AsyncState;
+                HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(resultat);
+                System.Diagnostics.Debug.WriteLine(response.ResponseUri);
+
+                foreach (Cookie cookieValue in response.Cookies)
+                {
+                    System.Diagnostics.Debug.WriteLine(cookieValue.ToString());
+                }
+
+            }, client);
+            */
+            MessageDialog messageDialog = new MessageDialog("Caligula arrivera dans une prochaine mise à jour");
+            await messageDialog.ShowAsync();
+            activerControles();
+
+
+        }
+        #endregion
+
+        #region Boutons et Champs de texte
+        private async void Connexion_Click(object sender, RoutedEventArgs e)
+        {
+            ERROR rs;
+            MessageDialog messageDialog;
+            desactiverControles();
+            StatusBar.GetForCurrentView().ProgressIndicator.ShowAsync();
+            try
+            {
+                rs = await APIWebTeam.BeginCheckLogin(login.Text, password.Password);
+            }
+            catch
+            {
+                messageDialog = new MessageDialog("Problème de connexion Internet. Reessayez");
+                messageDialog.ShowAsync();
+                rs = ERROR.TIMEOUT;
+                
+            }
+            switch (rs)
+            {
+
+                case ERROR.INCORRECT_LOGIN_OR_PWD:
+                    messageDialog = new MessageDialog("Login ou mot de passe incorrect.");
+                    await messageDialog.ShowAsync();
+                    goto default;
+                case ERROR.NO_LOGIN_OR_PWD:
+                    messageDialog = new MessageDialog("Login ou mot de passe manquant.");
+                    await messageDialog.ShowAsync();
+                    goto default;
+                case ERROR.NO_ERR:
+                    //goToWebTeamPage(this);
+                    goto default;
+                default:
+                    StatusBar.GetForCurrentView().ProgressIndicator.HideAsync();
+                    activerControles();
+                    break;
+            }
+        }
+
+        private void ConnexionCaligula_Click(object sender, RoutedEventArgs e)
+        {
+            desactiverControles();
+            BeginCaligulaLogin();
+        }
+
+
+        /* Donne le Focus au password quand on appuie sur la touche entrée */
+        private void login_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                password.Focus(FocusState.Programmatic);
+            }
+        }
+
+        /* Bouton entrée = tap connexion */
+        private void password_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Connexion_Click(this, null);
+            }
+        }
+
+        private void desactiverControles()
+        {
+            login.IsEnabled = false;
+            password.IsEnabled = false;
+            Connexion.IsEnabled = false;
+            ConnexionCaligula.IsEnabled = false;
+        }
+
+        private void activerControles()
+        {
+            login.IsEnabled = true;
+            password.IsEnabled = true;
+            Connexion.IsEnabled = true;
+            ConnexionCaligula.IsEnabled = true;
+        }
+
+        private void reinitialiserChamps()
+        {
+            login.Text = String.Empty;
+            password.Password = String.Empty;
+        }
+
+        private void SettingsAbout_ApplicationBarMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //NavigationService.Navigate(new Uri("/Settings.xaml", UriKind.Relative));
+        }
+
+        #endregion
+
+
     }
 }
