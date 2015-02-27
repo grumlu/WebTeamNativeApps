6540dffeda9622d99cc6f9835ee030d21fcfdcc9
Thomas Havy <grumluluche@gmail.com>
2015-02-27 18:38:40 +0100

    Ajout d'un helper avec des mots doux

---
+++ Windows/WebTeamWindows/WebTeamWindows.Shared/View/LoginViewHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebTeamWindows.View
+{
+    static class LoginViewHelper
+    {
+        public const string[] nickname ={
+                                   "eucalyptus",
+                                   "panda",
+                                   "maître",
+                                   "connard",
+                                   "étudiant",
+                                   "ourson",
+                                   "loup",
+                                   "rat",
+                                   "débile",
+                                   "mangeur de chaussettes puantes",
+                                   "petit ange",
+                                   "anchois"
+                               };
+
+        public const string[] adjective = {
+                                             "en chaleur",
+                                             "doux",
+                                             "vert",
+                                             "doré",
+                                             "argenté",
+                                             "sauvage",
+                                             "inutile",
+                                             "sexy",
+                                             "qui pue",
+                                             "qui sent bon",
+                                             "qu'on aime fort"
+                                        };
+    }
+}
